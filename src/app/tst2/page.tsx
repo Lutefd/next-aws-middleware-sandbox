e863cd6cbfeb1267d5ee2529c25ebee1fea811d7
luis dourado <lfaddourado@gmail.com>
2023-05-31 13:18:46 -0300

    feat: return to local

--- src/app/tst2/page.tsx
+++ src/app/tst2/page.tsx
@@ -6,16 +6,14 @@ import { Amplify } from 'aws-amplify';
 import awsExports from '@/aws-exports';
 
 Amplify.configure({ ...awsExports, ssr: true });
+
 async function Home() {
   const getData = async () => {
-    const res = await fetch(
-      'https://main.d32hankwz8sk07.amplifyapp.com/api/users/',
-      {
-        next: {
-          revalidate: 0,
-        },
-      }
-    );
+    const res = await fetch('http://localhost:3000/api/users/', {
+      next: {
+        revalidate: 0,
+      },
+    });
     const data = await res.json();
 
     return data.results;
