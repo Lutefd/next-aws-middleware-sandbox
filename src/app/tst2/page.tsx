d05c8796a95787211cf08d4cc896413b50cf7f00
luis dourado <lfaddourado@gmail.com>
2023-05-31 13:45:27 -0300

    feat: add fetching data

--- src/app/tst2/page.tsx
+++ src/app/tst2/page.tsx
@@ -8,23 +8,28 @@ import awsExports from '@/aws-exports';
 Amplify.configure({ ...awsExports, ssr: true });
 
 async function Home() {
-  // const getData = async () => {
-  //   const res = await fetch('http://localhost:3000/api/users/', {
-  //     next: {
-  //       revalidate: 0,
-  //     },
-  //   });
-  //   const data = await res.json();
-  //   return data.results;
-  // };
-  // const data = await getData();
-  // return (
-  //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  //     <Suspense fallback={<Loading />}>
-  //       <Users data={data} />
-  //     </Suspense>
-  //   </main>
-  // );
+  const getData = async () => {
+    const res = await fetch(
+      'https://main.d22u058tq4zno8.amplifyapp.com/api/users/',
+      {
+        next: {
+          revalidate: 0,
+        },
+      }
+    );
+    const data = await res.json();
+
+    return data.results;
+  };
+
+  const data = await getData();
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Suspense fallback={<Loading />}>
+        <Users data={data} />
+      </Suspense>
+    </main>
+  );
 }
 
 export default Home;
