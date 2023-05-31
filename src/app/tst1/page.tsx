e02d0c673404eb9526bab98e2c027b99f8e8af15
luis dourado <lfaddourado@gmail.com>
2023-05-31 13:38:23 -0300

    feat: prepare for deploying

--- src/app/tst1/page.tsx
+++ src/app/tst1/page.tsx
@@ -5,20 +5,19 @@ import awsExports from '@/aws-exports';
 Amplify.configure({ ...awsExports, ssr: true });
 
 export default async function Page() {
-  const getData = async () => {
-    const res = await fetch('http://localhost:3000/api/random/', {
-      next: {
-        revalidate: 0,
-      },
-    });
-    const data = await res.json();
-    return data.random;
-  };
-  const data = await getData();
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Numbers data={data} />
-    </main>
-  );
+  // const getData = async () => {
+  //   const res = await fetch('http://localhost:3000/api/random/', {
+  //     next: {
+  //       revalidate: 0,
+  //     },
+  //   });
+  //   const data = await res.json();
+  //   return data.random;
+  // };
+  // const data = await getData();
+  // return (
+  //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  //     <Numbers data={data} />
+  //   </main>
+  // );
 }
--- src/app/tst2/page.tsx
+++ src/app/tst2/page.tsx
@@ -8,25 +8,23 @@ import awsExports from '@/aws-exports';
 Amplify.configure({ ...awsExports, ssr: true });
 
 async function Home() {
-  const getData = async () => {
-    const res = await fetch('http://localhost:3000/api/users/', {
-      next: {
-        revalidate: 0,
-      },
-    });
-    const data = await res.json();
-
-    return data.results;
-  };
-
-  const data = await getData();
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Suspense fallback={<Loading />}>
-        <Users data={data} />
-      </Suspense>
-    </main>
-  );
+  // const getData = async () => {
+  //   const res = await fetch('http://localhost:3000/api/users/', {
+  //     next: {
+  //       revalidate: 0,
+  //     },
+  //   });
+  //   const data = await res.json();
+  //   return data.results;
+  // };
+  // const data = await getData();
+  // return (
+  //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  //     <Suspense fallback={<Loading />}>
+  //       <Users data={data} />
+  //     </Suspense>
+  //   </main>
+  // );
 }
 
 export default Home;
